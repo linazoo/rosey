1e9062494b3fd15e30f29c79012a8224d62a5e1c
Lina Zoo <linazooper@gmail.com>
2022-09-07 15:48:07 -0500

    styling the inputdetail component with mui

--- src/components/InputDetail.tsx
+++ src/components/InputDetail.tsx
@@ -3,18 +3,49 @@ import { ColorButton } from "./Directory";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import "./inputDetailStyles.css";
+import { Box } from "@mui/material";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
 
 const InputDetail = () => {
+  const [value, setValue] = React.useState(false);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: boolean) => {
+    setValue(newValue);
+  };
+
   return (
-    <form className="input">
-      <h3>Title</h3>
-      <input type="input" placeholder="enter a task"></input>
-      <h3>Description</h3>
-      <input type="input" placeholder="enter a task"></input>
+    <Container sx={{ display: "flex", flexDirection: "column" }}>
+      <TextField
+        id="standard-textarea"
+        label="Title"
+        multiline
+        variant="standard"
+      />
+      <TextField
+        id="standard-multiline-static"
+        label="Description"
+        multiline
+        rows={4}
+        defaultValue=""
+        variant="standard"
+      />
+      <Box sx={{ width: "100%" }}>
+        <Tabs
+          onChange={handleChange}
+          value={value}
+          aria-label="Tabs where each tab needs to be selected manually"
+        >
+          <Tab label="daily" />
+          <Tab label="weekly" />
+          <Tab label="monthly" />
+          <Tab label="yearly" />
+        </Tabs>
+      </Box>
       <ColorButton variant="contained" sx={{ width: "100%", mt: "20px" }}>
         Confirm
       </ColorButton>
-    </form>
+    </Container>
   );
 };
 
